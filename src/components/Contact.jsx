de747b55a849d7ee27c94332a91a5d5870eda08e
Timothé Joubert <timothe.joubert@hotmail.fr>
2021-06-30 13:10:39 +0200

    Ajout archive catalogue

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,18 @@
+import { useState } from "preact/hooks";
 import "./Contact.css";
 
 function Contact(props) {
+  const [currentCatalogue, setCurrentCatalogue] = useState();
   const data = props.data;
+  console.log(props.data);
 
   const contentObj = {
     __html: data.content.replace(/(?:\r\n|\r|\n)/g, "<br>"),
+  };
+
+  const handleChange = (e) => {
+    setCurrentCatalogue(e.target.value);
+    console.log(e.target.value);
   };
 
   return (
@@ -21,24 +29,53 @@ function Contact(props) {
             E-mail : <span>{data.content2}</span>
           </p>
         </div>
+
+        <h4 className="title-archive">Télécharger nos catalogues</h4>
+
         <div class="catalogue">
           <a href="https://www.abravinum.com/pdf/catalogue.pdf">
-            Accès au catalogue
+            Saison Automne 2019
           </a>
+        </div>
+
+        <div className="block-archive">
+          <div id="archive-container">
+            <select
+              name="archive"
+              id="archive-select"
+              value={currentCatalogue}
+              onChange={handleChange}
+            >
+              <option selected disabled>
+                Saisons
+              </option>
+              <option value="hiver-2019">Hiver 2019</option>
+              <option value="ete-2020">Été 2020</option>
+            </select>
+            <a
+              href={
+                currentCatalogue &&
+                `https://www.abravinum.com/pdf/catalogue-${currentCatalogue}.pdf`
+              }
+              style={
+                currentCatalogue
+                  ? { background: "#222222" }
+                  : { cursor: "inherit", color: "rgb(191, 191, 191)" }
+              }
+              download={`catalogue-${currentCatalogue}.pdf`}
+              id="download-archive-btn"
+            >
+              Télécharger
+            </a>
+          </div>
         </div>
       </section>
 
       <footer>
         <div class="reseaux">
-          {/* <a href="">
-            <span>Facebook</span>
-          </a>
-          <a href="">
-            <span>Twitter</span>
-          </a>
-          <a href="">
-            <span>Instagram</span>
-          </a> */}
+          {/* <a href=""><span>Facebook</span></a>
+          <a href=""><span>Twitter</span></a>
+          <a href=""><span>Instagram</span></a> */}
         </div>
         <div class="retour-top scroll-link">
           <a href="#home">&#8593;</a>
